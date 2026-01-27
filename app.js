6e593612f7dfb600335b2aa7cca7ccadbfdfdc68
Julie Wendelboe Bøcher <juliewboecher@gmail.com>
2026-01-27 10:44:29 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// ===== APP INITIALISERING =====
+// Start app når DOM er loaded (hele HTML siden er færdig med at indlæse)
+document.addEventListener("DOMContentLoaded", initApp);
+
+// Initialize the app - sæt event listeners og hent data
+function initApp() {
+  console.log("App initialized");
+}
