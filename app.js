cc4874ae78f403436b6ac60e76060a9c74ec5a7d
Julie Wendelboe Bøcher <juliewboecher@gmail.com>
2026-01-27 20:59:48 +0100

    Byt True og false

--- app.js
+++ app.js
@@ -12,35 +12,38 @@ function initApp() {
 // tjekker om console virker
 console.log("Tjekker om console virker");
 
+// Del 1
 // Tilføjer string, number og boolean. Typeof viser hvilken type variabel det er
 const productTitle ="Fjalren  - Foldsack No. 1 Backpack";
 const productPrice = 849;
-const inStock = true;
-
+const inStock = false;
+// Del 2
 console.log(typeof productTitle);
 console.log(typeof productPrice);
 console.log(typeof inStock);
 
+// OPGAVE 3
 // Js til bunden af side (test sektion)
+if (inStock) { // tilføjet fra opgave 4
 const testSection = document.querySelector("#test");
 const html = `
 <div>
   <h3>Test: Product Information</h3>
   <p><strong>Titel:</strong>${productTitle}</p>
-  <p><strong>Pris:</strong>${productPrice}</p>
+  <p><strong>Pris:</strong>${productPrice} kr.</p>
   <p><strong>På lager:</strong>${inStock}</p>
 </div>
 `;
-
 testSection.insertAdjacentHTML("beforeend", html);
+}
 
 // Nye variabler
 const productTitle2 = "Mens Casual Premium Slim Fit T-Shirt";
 const productPrice2 = 199;
-const inStock2 = false;
+const inStock2 = true;
 
 console.log(typeof productTitle2);
-console.log(typeof produktPrice2);
+console.log(typeof productPrice2);
 console.log(typeof inStock2);
 
 const testSection2 = document.querySelector("#test");
@@ -48,7 +51,7 @@ const html2 = `
 <div>
   <h3>Test: Product Information</h3>
   <p><strong>Titel:</strong>${productTitle2}</p>
-  <p><strong>Pris:</strong>${productPrice2}</p>
+  <p><strong>Pris:</strong>${productPrice2} kr.</p>
   <p><strong>På lager:</strong>${inStock2}</p>
 </div>
 `;
@@ -60,6 +63,6 @@ if (inStock) {
   console.log(`${productTitle} er på lager`);
 }
 else {
-  console.log(`${productTitle} er ikke på lager`);
+  console.log(`${productTitle} er desværre ikke på lager!`);
 }
 
