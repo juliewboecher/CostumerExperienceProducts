0fd8e7d8f8e7417ff377b7bfa2d47600fba7de09
Julie Wendelboe Bøcher <juliewboecher@gmail.com>
2026-01-27 20:48:11 +0100

    rem fra 2-4, indsæt true/false om den er på lager

--- app.js
+++ app.js
@@ -8,3 +8,58 @@ document.addEventListener("DOMContentLoaded", initApp);
 function initApp() {
   console.log("App initialized");
 }
+
+// tjekker om console virker
+console.log("Tjekker om console virker");
+
+// Tilføjer string, number og boolean. Typeof viser hvilken type variabel det er
+const productTitle ="Fjalren  - Foldsack No. 1 Backpack";
+const productPrice = 849;
+const inStock = true;
+
+console.log(typeof productTitle);
+console.log(typeof productPrice);
+console.log(typeof inStock);
+
+// Js til bunden af side (test sektion)
+const testSection = document.querySelector("#test");
+const html = `
+<div>
+  <h3>Test: Product Information</h3>
+  <p><strong>Titel:</strong>${productTitle}</p>
+  <p><strong>Pris:</strong>${productPrice}</p>
+  <p><strong>På lager:</strong>${inStock}</p>
+</div>
+`;
+
+testSection.insertAdjacentHTML("beforeend", html);
+
+// Nye variabler
+const productTitle2 = "Mens Casual Premium Slim Fit T-Shirt";
+const productPrice2 = 199;
+const inStock2 = false;
+
+console.log(typeof productTitle2);
+console.log(typeof produktPrice2);
+console.log(typeof inStock2);
+
+const testSection2 = document.querySelector("#test");
+const html2 = `
+<div>
+  <h3>Test: Product Information</h3>
+  <p><strong>Titel:</strong>${productTitle2}</p>
+  <p><strong>Pris:</strong>${productPrice2}</p>
+  <p><strong>På lager:</strong>${inStock2}</p>
+</div>
+`;
+
+testSection.insertAdjacentHTML("beforeend", html2);
+
+// Vis hvis på lager
+if (inStock) {
+  console.log(`${productTitle} er på lager`);
+}
+else {
+  console.log(`${productTitle} er ikke på lager`);
+}
+
